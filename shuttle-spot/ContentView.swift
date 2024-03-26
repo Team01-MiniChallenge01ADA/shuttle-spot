0d98279ff4d1958a1e72354fee19ae4d812eff06
DasonTio <d4sontiovino@gmail.com>
2024-03-26 16:15:41 +0700

    Initial Commit

---
+++ shuttle-spot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shuttle-spot
+//
+//  Created by Dason Tiovino on 26/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shuttle-spot/shuttle_spotApp.swift
@@ -0,0 +1,17 @@
+//
+//  shuttle_spotApp.swift
+//  shuttle-spot
+//
+//  Created by Dason Tiovino on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct shuttle_spotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
