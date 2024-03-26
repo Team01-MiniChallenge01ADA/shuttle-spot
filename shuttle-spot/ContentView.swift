4207a3fd4704eb014dbcf61dc16d7b6b0a8cd0a6
DasonTio <d4sontiovino@gmail.com>
2024-03-26 17:06:37 +0700

    init

--- shuttle-spot/ContentView.swift
+++ shuttle-spot/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader{ geometry in
+            VStack(alignment: .leading){
+                HStack{
+                    VStack(alignment: .leading){
+                        HStack(spacing: 0){
+                            Text("Hello, ")
+                            Text("Dason Tiovino")
+                                .font(.headline)
+                        }
+                        HStack(spacing:10){
+                            Image(systemName: "pin.figll")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 10)
+                                .foregroundColor(.gray)
+                            Text("Indonesia, Jakarta")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    Spacer()
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:geometry.size.width/100 * 10)
+                }.padding()
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.white)
+                        .stroke(Color.gray.opacity(0.3))
+                        .frame(height:70)
+
+                    VStack(alignment: .leading){
+                        Text("Court Booking Reminder")
+                            .font(.caption)
+                        Text("12 March 2024")
+                            .font(.caption)
+                    }
+                }.padding(.horizontal)
+            }.frame(width: geometry.size.width, height: geometry.size.height, alignment: .topLeading)
         }
-        .padding()
     }
 }
 
